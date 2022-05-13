b39e46348d61c7dba0ce130e8745287c645fcbc2
valeriyKH <valera91k@yandex.ru>
2022-05-13 11:06:25 +0300

    Final commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,17 +2,26 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let Discrim = Math.pow(b ^ 2) - (4 * a * c);
-  arr.push(Discrim);
-  console.log(arr);
+  let Discrim = Math.pow(b ^ 2 - 4 * a * c);
+  let root1;
+  let root2;
 
-  if (Discrim < 0) {
+  if (Discrim > 0) {
+    root1 = (- b + Math.sqrt(Discrim) ) / (2 * a);
+    root2 = (- b - Math.sqrt(Discrim) ) / (2 * a);
+
+    arr.push(root1.toFixed(0));
+    arr.push(root2.toFixed(0));
     console.log(arr);
-  } else if (Discrim = 0) {
-    console.log("1 Root = " + (-b) / (2 * a));
-  } else if (Discrim > 0) {
-    console.log(["1 Root = " + (-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]);    
-    }
-};
+  } else if (Discrim === 0) {
+    root1 = (-b / (2 * a));
+    arr.push(root1.toFixed(0));
+    console.log(arr);
+  } else  {
+    console.log(arr);
+  }
+};  
+  
+  
 
-solveEquation();
+solveEquation(1, 5, 4);
